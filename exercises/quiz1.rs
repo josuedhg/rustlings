4c4cff98ccae1fc02ff968418a80df11425145cf
Josue David Hernandez Gutierrez <megadavidtor@gmail.com>
2023-08-07 15:36:58 -0600

    (done) quiz1
    Signed-off-by: Josue David Hernandez Gutierrez <megadavidtor@gmail.com>

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,11 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount: u32) -> u32 {
+    let cost = if amount > 40 { 1 } else { 2 };
+    amount * cost
+}
 
 // Don't modify this function!
 #[test]
